a1086b440af07a6495e398bc685fc87e6b5b2533
pfe1223 <patrickester@APP-1554.local>
2016-07-07 08:17:42 -0700

    If-else Statements

---
+++ basics/voting.py
@@ -0,0 +1,13 @@
+# All indented lines in an "if" or "else"statements will be executed when the appropriate conditions
+# are met
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote.")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18.")
+
+
+    
